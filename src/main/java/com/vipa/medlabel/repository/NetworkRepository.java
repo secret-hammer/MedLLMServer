cdeb06e796ad1f9949e06df0bfcced86ff4621f5
Jinxun Wu <jinxunwu@zju.edu.cn>
2024-07-31 12:14:45 +0000

    delete: 清除无关数据模型

--- src/main/java/com/vipa/medlabel/repository/NetworkRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vipa.medlabel.repository;
-
-import com.vipa.medlabel.model.Network;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NetworkRepository extends JpaRepository<Network, Integer> {
-    // Additional custom queries can be defined here
-}

eb52515713eed14ce5d959f3131d40955bfead89
Jinxun Wu <jinxunwu@zju.edu.cn>
2024-08-01 10:45:37 +0000

    config: 增加spring retry config

---
+++ src/main/java/com/vipa/medllm/config/retryconfig/RetryConfig.java
@@ -0,0 +1,10 @@
+package com.vipa.medllm.config.retryconfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@Configuration
+@EnableRetry
+public class RetryConfig {
+    // 暂时不配置复杂重试策略
+}

b5207f35659ca323ca2d8ae6ef2580c9dee3df7b
Jinxun Wu <jinxunwu@zju.edu.cn>
2024-08-17 04:02:59 +0000

    add: 增加图片url存在性判断

--- src/main/java/com/vipa/medllm/util/ImageValidator.java
+++ src/main/java/com/vipa/medllm/util/ImageValidator.java
@@ -1,21 +1,32 @@
 package com.vipa.medllm.util;
 
 import java.util.List;
-import java.util.stream.Stream;
 
-import com.vipa.medllm.repository.ImageTypeRepository;
+import com.vipa.medllm.model.ImageType;
+import com.alibaba.fastjson.JSON;
 import org.springframework.stereotype.Component;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Component
 public class ImageValidator {
 
-    public boolean isValidImage(String imageUrl, ImageTypeRepository imageTypeRepository) {
+    public boolean isValidImage(String imageUrl, ImageType imageType) {
+         // 检查文件是否存在
+         if (!Files.exists(Paths.get(imageUrl))) {
+            return false;
+        }
+
         String fileExtension = getFileExtension(imageUrl);
-//        List<String> validExtensions = imageTypeRepository.findAllExtensions().stream()
-//                .flatMap(ext -> Stream.of(ext.split(","))).toList();
-        // 指定为【"tif", "mrxs"】
-        List<String> validExtensions = List.of("tif", "mrxs");
-        return validExtensions.contains(fileExtension);
+        String imageTypeExtension = imageType.getImageExtensions();
+        List<String> allowedExtensions = JSON.parseArray(imageTypeExtension, String.class);
+
+        // 检查文件的扩展名是否在允许的文件后缀列表中
+        if (!allowedExtensions.contains(fileExtension)) {
+            return false;
+        }
+        // 文件的扩展名在允许的文件后缀列表中，且文件存在，文件是有效的
+        return true;
     }
 
     private String getFileExtension(String imageUrl) {
